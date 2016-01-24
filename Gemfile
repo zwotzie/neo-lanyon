683ede28a7457208623769ffbf8d3a903ad31fd7
zwotzie <rh@web.de>
2016-01-24 21:43:26 +0100

    autobuild

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "https://rubygems.org"
+
+gem 'wdm', '>= 0.1.0'
+gem 'jekyll'
+gem 'jekyll-sitemap'
+gem 'octopress', '~> 3.0'
+gem 'jekyll-paginate'
---
+++ gruntfile.js
@@ -0,0 +1,110 @@
+'use strict';
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: [
+        'Gruntfile.js',
+        'assets/js/*.js',
+        'assets/js/plugins/*.js',
+        '!assets/js/scripts.min.js'
+      ]
+    },
+    uglify: {
+      dist: {
+        files: {
+          'assets/js/scripts.min.js': [
+            'assets/js/plugins/*.js',
+            'assets/js/_*.js'
+          ]
+        }
+      }
+    },
+    imagemin: {
+      dist: {
+        options: {
+          optimizationLevel: 7,
+          progressive: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'images/',
+          src: '{,*/}*.{png,jpg,jpeg}',
+          dest: 'images/'
+        }]
+      }
+    },
+    svgmin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'images/',
+          src: '{,*/}*.svg',
+          dest: 'images/'
+        }]
+      }
+    },
+    watch: {
+      js: {
+        files: [
+          '<%= jshint.all %>'
+        ],
+        tasks: ['jshint','uglify']
+      }
+    },
+    clean: {
+      dist: [
+        'assets/js/scripts.min.js'
+      ]
+    },
+    buildcontrol: {
+        options: {
+            dir: '_site',
+            commit: true,
+	        connectCommits: false,
+            push: true,
+            message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+        },
+        pages: {
+            options: {
+                remote: 'https://github.com/zwotzie/neo-lanyon.git', // change that
+                branch: 'gh-pages' // adjust here, 'source' for user page and or 'gh-pages' for project pages
+            }
+        }
+    },
+    shell: {
+        jekyllServe: {
+            command: "jekyll serve  --no-watch"
+        },
+        jekyllBuild: {
+            command: "jekyll build"
+        }
+    }
+  });
+
+  // Load tasks
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-svgmin');
+  grunt.loadNpmTasks('grunt-build-control');
+
+  // Register tasks
+  grunt.registerTask('default', [
+    'clean',
+    'uglify',
+    'imagemin',
+    'svgmin'
+  ]);
+  grunt.registerTask('dev', [
+    'watch'
+  ]);
+  grunt.registerTask('deploy', [
+    'buildcontrol:pages'
+  ]);
+};
